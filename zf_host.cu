ea9161301bf1505791d70beda4403983527c6d83
Honpray <hanbing.leng@sjsu.edu>
2019-05-07 11:54:38 -0700

    Update conv function

--- zf_host.cu
+++ zf_host.cu
@@ -54,9 +54,15 @@ int main(int argc, char **argv) {
   float *d_layer_1_pooled;
   float *d_layer_1_padded;
 
-  cudaMalloc((void **)&d_input, INPUT_SIZE * sizeof(float));
-  cudaMalloc((void **)&d_layer_1_input, LAYER_1_INPUT_SIZE * sizeof(float));
+  float *d_layer_2_input;
+  float *d_layer_2_weights;
+  float *d_layer_2_pooled;
+  float *d_layer_2_padded;
 
+  // input 
+  cudaMalloc((void **)&d_input, INPUT_SIZE * sizeof(float));
+  // layer 1
+  cudaMalloc((void **)&d_layer_1_input, LAYER_1_INPUT_SIZE * sizeof(float));
   cudaMalloc((void **)&d_layer_1_weights, LAYER_1_FILTER_SIZE * LAYER_1_FILTER_NUM * sizeof(float));
   cudaMemcpy(d_layer_1_weights, layer_1_weights, LAYER_1_FILTER_SIZE * LAYER_1_FILTER_NUM * sizeof(float), cudaMemcpyHostToDevice);
 
@@ -65,6 +71,17 @@ int main(int argc, char **argv) {
   cudaMalloc((void **)&d_layer_1_padded, LAYER_1_PADDED_SIZE * sizeof(float));
   cudaMemset(d_layer_1_padded, 0, LAYER_1_POOLED_SIZE * sizeof(float));
 
+  // layer 2
+  cudaMalloc((void **)&d_layer_2_input, LAYER_2_INPUT_SIZE * sizeof(float));
+
+  cudaMalloc((void **)&d_layer_2_weights, LAYER_2_FILTER_SIZE * LAYER_2_FILTER_NUM * sizeof(float));
+  cudaMemcpy(d_layer_2_weights, layer_2_weights, LAYER_2_FILTER_SIZE * LAYER_2_FILTER_NUM * sizeof(float), cudaMemcpyHostToDevice);
+
+  cudaMalloc((void **)&d_layer_2_pooled, LAYER_2_POOLED_SIZE * sizeof(float));
+  
+  cudaMalloc((void **)&d_layer_2_padded, LAYER_2_PADDED_SIZE * sizeof(float));
+  cudaMemset(d_layer_2_padded, 0, LAYER_2_POOLED_SIZE * sizeof(float));
+
 
 
   // layer 1: 110 * 110 * 96
@@ -72,7 +89,8 @@ int main(int argc, char **argv) {
   dim3 conv_1_block_dim(110, 110);
 
   printf("Running conv_1 ...\n");
-  run_conv_1<<<conv_1_grid_dim, conv_1_block_dim>>>(d_input, d_layer_1_weights, d_layer_1_input);
+  // stride 2, filter size 7, channel_num 3, input_width 224, output_width 110
+  run_conv_1<<<conv_1_grid_dim, conv_1_block_dim>>>(d_input, d_layer_1_weights, d_layer_1_input, 2, 7, 3, 224, 110);
 
   dim3 pool_1_grid_dim(96, 1, 1);
   dim3 pool_1_block_dim(55, 55);
@@ -82,12 +100,36 @@ int main(int argc, char **argv) {
   dim3 pad_1_grid_dim(96, 1, 1);
   dim3 pad_1_block_dim(55, 55);
   printf("Padding pool_1 output ...\n");
-  run_padding_1<<<pad_1_grid_dim, pad_1_block_dim>>>(d_layer_1_pooled, d_layer_1_padded);
+  run_padding_1<<<pad_1_grid_dim, pad_1_block_dim>>>(d_layer_1_pooled, d_layer_1_padded, 55);
 
   dim3 lcn_1_grid_dim(96, 1, 1);
   dim3 lcn_1_block_dim(55, 55);
   printf("Running lcn_1\n");
-  run_lcn_1<<<lcn_1_grid_dim, lcn_1_block_dim>>>(d_layer_1_padded);
+  run_lcn_1<<<lcn_1_grid_dim, lcn_1_block_dim>>>(d_layer_1_padded, d_layer_1_pooled);
+
+  // layer 2: 26 * 26 * 256
+  dim3 conv_2_grid_dim(256, 1, 1);
+  dim3 conv_2_block_dim(26, 26);
+
+  printf("Running conv_2 ...\n");
+  run_conv_2<<<conv_2_grid_dim, conv_2_block_dim>>>(d_input, d_layer_2_weights, d_layer_2_input);
+
+  dim3 pool_2_grid_dim(256, 1, 1);
+  dim3 pool_2_block_dim(26, 26);
+  printf("Running pool_2 ...\n");
+  run_pool_2<<<pool_2_grid_dim, pool_2_block_dim>>>(d_layer_2_input, d_layer_2_pooled);
+
+  dim3 pad_2_grid_dim(256, 1, 1);
+  dim3 pad_2_block_dim(26, 26);
+  printf("Padding pool_2 output ...\n");
+  run_padding_2<<<pad_2_grid_dim, pad_2_block_dim>>>(d_layer_2_pooled, d_layer_2_padded, 26);
+
+  dim3 lcn_2_grid_dim(256, 1, 1);
+  dim3 lcn_2_block_dim(26, 26);
+  printf("Running lcn_2\n");
+  run_lcn_2<<<lcn_2_grid_dim, lcn_2_block_dim>>>(d_layer_2_padded, d_layer_2_pooled);
+
+
 }
 
 
--- zf_kernel.cu
+++ zf_kernel.cu
@@ -1,24 +1,22 @@
 #include <math.h>
 
-__global__ void run_conv_1(float *d_input, float *d_layer_1_weights, float *d_layer_1_input) {
-  // stride 2, filter size 7
+__global__ void run_conv_1(float *d_input, float *d_layer_1_weights, float *d_layer_1_input, int stride, int filter_size, int channel_num, int input_width, int output_width) {
   float product = 0;
-  int stride = 2;
   int tx = threadIdx.x;
   int ty = threadIdx.y;
   int bx = blockIdx.x;
-  for (int i = 0; i < 7; i++) {
-    for (int j = 0; j < 7; j++) {
-      product += d_input[3 * (i * 224 + j + tx * stride + ty * 224 * stride)] * d_layer_1_weights[i * 11 + j + bx * 11 * 11 * 3 + 11 * 11];
-      product += d_input[3 * (i * 224 + j + tx * stride + ty * 224 * stride) + 1] * d_layer_1_weights[i * 11 + j + bx * 11 * 11 * 3 + 11 * 11 * 2];
-      product += d_input[3 * (i * 224 + j + tx * stride + ty * 224 * stride) + 2] * d_layer_1_weights[i * 11 + j + bx * 11 * 11 * 3 + 11 * 11 * 3];
+  for (int k = 1; k <= channel_num; k++) {
+    for (int i = 0; i < filter_size; i++) {
+      for (int j = 0; j < filter_size; j++) {
+        product += d_input[channel_num * (i * input_width + j + tx * stride + ty * input_width * stride) + k] * d_layer_1_weights[i * filter_size + j + bx * filter_size * filter_size * channel_num + filter_size * filter_size * k];
+      }
     }
   }
   // ReLU
   if (product < 0) {
     product = 0;
   }
-  d_layer_1_input[tx + ty * 110 + bx * 110 * 110] = product;
+  d_layer_1_input[tx + ty * output_width + bx * output_width * output_width] = product;
   product = 0;
 }
 
@@ -41,11 +39,11 @@ __global__ void run_pool_1(float *d_layer_1_input, float *d_layer_1_pooled) {
   max = 0;
 }
 
-__global__ void run_padding_1(float *d_layer_1_pooled, float *d_layer_1_padded) {
+__global__ void run_padding_1(float *d_layer_1_pooled, float *d_layer_1_padded, int width) {
   int tx = threadIdx.x;
   int ty = threadIdx.y;
   int bx = blockIdx.x;
-  d_layer_1_padded[(ty + 1) * 55 + (tx + 1) + bx * 55 * 55] = d_layer_1_pooled[ty * 55 + tx + bx * 55 * 55];
+  d_layer_1_padded[(ty + 1) * width + (tx + 1) + bx * width * width] = d_layer_1_pooled[ty * width + tx + bx * width * width];
 }
 
 __global__ void run_lcn_1(float *d_layer_1_padded, float *d_layer_1_pooled) {
